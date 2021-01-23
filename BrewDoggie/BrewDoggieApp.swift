2cd85c6d6b2fb42cd6990f1d3cbd5743a4d8047e
Marek Repinski <Repinski@hotmail.com>
2021-01-23 12:54:41 +0100

    Initial Commit

---
+++ BrewDoggie/BrewDoggieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrewDoggieApp.swift
+//  BrewDoggie
+//
+//  Created by Marek Repinski on 2021-01-23.
+//
+
+import SwiftUI
+
+@main
+struct BrewDoggieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
